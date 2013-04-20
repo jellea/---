ad3d4626944685ef6255a273181520dd0393a43b
Guido Bouman <info@guidobouman.com>
2013-04-20 19:52:56 +0200

    Chart uses bitcoin data now.

--- js/chart.js
+++ js/chart.js
@@ -1,14 +1,32 @@
 $(function() {
 
-    var sin = [], cos = [];
-    for (var i = 0; i < 14; i += 0.1) {
-        sin.push([i, Math.sin(i)]);
-        cos.push([i, Math.cos(i)]);
+    var open = [],
+        high = [],
+        low = [],
+        close = [],
+        volume_btc = [],
+        volume_cur = [],
+        price = [];
+    for (var i = 0; i < bitchart.length; i += 1) {
+        open.push([i, bitchart[i][1]]);
+        high.push([i, bitchart[i][2]]);
+        low.push([i, bitchart[i][3]]);
+        close.push([i, bitchart[i][4]]);
+        volume_btc.push([i, bitchart[i][5]]);
+        volume_cur.push([i, bitchart[i][6]]);
+        price.push([i, bitchart[i][7]]);
     }
 
+    console.log(open);
+
     plot = $.plot("#placeholder", [
-        { data: sin, label: "sin(x) = -0.00"},
-        { data: cos, label: "cos(x) = -0.00" }
+        { data: open, label: "open"},
+        { data: high, label: "high"},
+        { data: low, label: "low"},
+        { data: close, label: "close"},
+        { data: volume_btc, label: "volume_btc"},
+        { data: volume_cur, label: "volume_cur"},
+        { data: price, label: "price"}
     ], {
         series: {
             lines: {
@@ -23,68 +41,68 @@ $(function() {
             autoHighlight: false
         },
         yaxis: {
-            min: -1.2,
-            max: 1.2
+            min: 0,
+            max: 500
         }
     });
 
-    var legends = $("#placeholder .legendLabel");
+    // var legends = $("#placeholder .legendLabel");
 
-    legends.each(function () {
-        // fix the widths so they don't jump around
-        $(this).css('width', $(this).width());
-    });
+    // legends.each(function () {
+    //     // fix the widths so they don't jump around
+    //     $(this).css('width', $(this).width());
+    // });
 
-    var updateLegendTimeout = null;
-    var latestPosition = null;
+    // var updateLegendTimeout = null;
+    // var latestPosition = null;
 
-    function updateLegend() {
+    // function updateLegend() {
 
-        updateLegendTimeout = null;
+    //     updateLegendTimeout = null;
 
-        var pos = latestPosition;
+    //     var pos = latestPosition;
 
-        var axes = plot.getAxes();
-        if (pos.x < axes.xaxis.min || pos.x > axes.xaxis.max ||
-            pos.y < axes.yaxis.min || pos.y > axes.yaxis.max) {
-            return;
-        }
+    //     var axes = plot.getAxes();
+    //     if (pos.x < axes.xaxis.min || pos.x > axes.xaxis.max ||
+    //         pos.y < axes.yaxis.min || pos.y > axes.yaxis.max) {
+    //         return;
+    //     }
 
-        var i, j, dataset = plot.getData();
-        for (i = 0; i < dataset.length; ++i) {
+    //     var i, j, dataset = plot.getData();
+    //     for (i = 0; i < dataset.length; ++i) {
 
-            var series = dataset[i];
+    //         var series = dataset[i];
 
-            // Find the nearest points, x-wise
+    //         // Find the nearest points, x-wise
 
-            for (j = 0; j < series.data.length; ++j) {
-                if (series.data[j][0] > pos.x) {
-                    break;
-                }
-            }
+    //         for (j = 0; j < series.data.length; ++j) {
+    //             if (series.data[j][0] > pos.x) {
+    //                 break;
+    //             }
+    //         }
 
-            // Now Interpolate
+    //         // Now Interpolate
 
-            var y,
-                p1 = series.data[j - 1],
-                p2 = series.data[j];
+    //         var y,
+    //             p1 = series.data[j - 1],
+    //             p2 = series.data[j];
 
-            if (p1 === null) {
-                y = p2[1];
-            } else if (p2 === null) {
-                y = p1[1];
-            } else {
-                y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
-            }
+    //         if (p1 === null) {
+    //             y = p2[1];
+    //         } else if (p2 === null) {
+    //             y = p1[1];
+    //         } else {
+    //             y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
+    //         }
 
-            legends.eq(i).text(series.label.replace(/=.*/, "= " + y.toFixed(2)));
-        }
-    }
+    //         legends.eq(i).text(series.label.replace(/=.*/, "= " + y.toFixed(2)));
+    //     }
+    // }
 
-    $("#placeholder").bind("plothover",  function (event, pos, item) {
-        latestPosition = pos;
-        if (!updateLegendTimeout) {
-            updateLegendTimeout = setTimeout(updateLegend, 50);
-        }
-    });
+    // $("#placeholder").bind("plothover",  function (event, pos, item) {
+    //     latestPosition = pos;
+    //     if (!updateLegendTimeout) {
+    //         updateLegendTimeout = setTimeout(updateLegend, 50);
+    //     }
+    // });
 });
