e24d1b8f2e2cc8368a909fed0ea7c69d4f4ed10f
Jelle Akkerman <jelle@jelleakkerman.nl>
2013-04-20 18:48:24 +0200

    merging

--- script.js
+++ script.js
@@ -9,23 +9,37 @@ function parseData ()
 
 function playData (input)
 {
-  console.log(input);
+  setInterval(function() {
+    bitvalue = bitchart.shift();
+    if (bitvalue != undefined){
+      document.getElementsByTagName('p')[0] = bitvalue[0].format("dddd, MMMM Do YYYY")
+      for (var i=1;i<3;i++)
+      {
+        osc[i].noteOn(0);
+        osc[i].frequency.value = bitvalue[i]*10;
+      }
+    }else{
+      for (var i=1;i<3;i++)
+      {
+        osc[i].noteOff(0);
+      }
+    }
+  },400);
 }
 
 function initAudio()
 {
-  if (typeof AudioContext !== "undefined") {
-    myAudioContext = new AudioContext();
-  } else if (typeof webkitAudioContext !== "undefined") {
-    myAudioContext = new webkitAudioContext();
-  } else {
-    throw new Error('AudioContext not supported. :(');
+  if('webkitAudioContext' in window)
+    {
+      window.myAudioContext = new webkitAudioContext();
+    }
+  window.osc = {}
+  for (var i=1;i<6;i++)
+  {
+    osc[i] = myAudioContext.createOscillator();
+    osc[i].type = 0; // sine wave
+    osc[i].connect(myAudioContext.destination);
   }
-
-  window.source = myAudioContext.createOscillator();
-  source.type = 0; // sine wave
-
-  source.connect(myAudioContext.destination);
 }
 
 function init ()
