5074427e5fa4a72759430e49bec546d539e5dbf8
Jelle Akkerman <jelle@jelleakkerman.nl>
2013-04-20 21:07:33 +0200

    recorder is working!

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function playData (input)
   setInterval(function() {
     bitvalue = bitchart.shift();
     if (bitvalue != undefined){
-      document.getElementsByTagName('p')[0] = bitvalue[0].format("dddd, MMMM Do YYYY")
+      $('p').text(bitvalue[0].format("dddd, MMMM Do YYYY"));
       for (var i=1;i<3;i++)
       {
         osc[i].noteOn(0);
@@ -22,6 +22,7 @@ function playData (input)
       for (var i=1;i<3;i++)
       {
         osc[i].noteOff(0);
+        rec.stop();
       }
     }
   },400);
@@ -32,20 +33,27 @@ function initAudio()
   if('webkitAudioContext' in window)
     {
       window.myAudioContext = new webkitAudioContext();
+      window.gain = myAudioContext.createGainNode();
+      gain.gain.value = 5.0;
+      gain.connect(myAudioContext.destination);
+      var recorder = new Recorder(gain);
+      window.osc = {};
+      for (var i=1;i<6;i++)
+      {
+        osc[i] = myAudioContext.createOscillator();
+        osc[i].type = 0; // sine wave
+        osc[i].connect(gain);
+      }
+      rec = new Recorder(gain, {workerPath: "js/Recorderjs/recorderWorker.js"});
+      rec.record();
+      playData(parseData());
     }
-  window.osc = {}
-  for (var i=1;i<6;i++)
-  {
-    osc[i] = myAudioContext.createOscillator();
-    osc[i].type = 0; // sine wave
-    osc[i].connect(myAudioContext.destination);
-  }
+;
 }
 
 function init ()
 {
   initAudio();
-  playData(parseData());
 }
 
 window.onload = function()
